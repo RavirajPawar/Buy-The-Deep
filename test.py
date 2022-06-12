014d1c62cbc63b6c58ddd24b79695b93ec3eeca1
Raviraj <pawarraviraj9160@gmail.com>
2022-06-12 15:32:00 +0530

    updates for weekly high low

--- test.py
+++ test.py
@@ -16,15 +16,14 @@ def get_weekly_high_low(data, window_of_days=7):
         # sorting weekly_data in ascending order with help of price
         weekly_data = sorted(weekly_data, key=lambda dayly_data: dayly_data[1])
         weekly_high_low_data[i+1] = (weekly_data[-1], weekly_data[0])
-    print(weekly_high_low_data.get(99))
+        print("week", i+1, (weekly_data[-1], weekly_data[0]))
 
 
 HISTORICAL_DATA_API = "https://api.coingecko.com/api/v3/coins/{}/market_chart?vs_currency=inr&days={}"
 
-api_url = HISTORICAL_DATA_API.format("uniswap", 100)
+api_url = HISTORICAL_DATA_API.format("uniswap", 10000)
 api_response = requests.get(api_url)
 
 # reversing data for making weekly high low batch
 data = api_response.json()["prices"][:]
-print(len(data), data[0])
 get_weekly_high_low(data[::-1])
